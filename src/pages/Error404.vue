e64292a7a709c3096578050e5be86a15ca401100
YaProof <proudeagle@bk.ru>
2020-05-05 18:42:16 +0300

    Change not found page

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -7,13 +7,13 @@
       >
     </p>
     <p class="text-faded">
-      Sorry, nothing here...<strong>(404)</strong>
+      Извините, не удалось найти страницу...<strong>(404)</strong>
     </p>
     <q-btn
       color="secondary"
       style="width:200px;"
       to="/"
-      label="Go back"
+      label="Вернуться на главную"
     />
   </div>
 </template>
