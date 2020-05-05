7f64daf4ec9d30b991f0a30a2c854881817ec81d
YaProof <proudeagle@bk.ru>
2020-05-05 18:48:11 +0300

    Add stub auth page

---
+++ src/pages/Authorization.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Stub authorization page</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'AuthorizationPage'
+}
+</script>
