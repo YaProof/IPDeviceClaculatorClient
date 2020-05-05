0b2f24d2815d87d1f286d5f50db973afa19e224b
YaProof <proudeagle@bk.ru>
2020-05-05 18:44:04 +0300

    Add forbidden page

---
+++ src/pages/Forbidden.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fixed-center text-center">
+    <p>
+      <img
+        src="~assets/sad.svg"
+        style="width:30vw;max-width:150px;"
+      >
+    </p>
+    <p class="text-faded">
+      Извините, у вас не хватает прав для просмотра этой страницы
+    </p>
+    <q-btn
+      color="secondary"
+      style="width:200px;"
+      to="/"
+      label="Вернуться на главную"
+    />
+  </div>
+</template>
+
+<script >
+export default {
+  name: 'Forbidden'
+}
+</script>
